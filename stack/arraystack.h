3b82a9982f52f04768e956397f5b98fb5e0a92ce
Hyeonjin Jung <hyejung@c1r3s1.42seoul.kr>
2022-04-22 17:02:14 +0900

    stack

---
+++ stack/arraystack.h
@@ -0,0 +1,32 @@
+#ifndef _ARRAY_STACK_
+#define _ARRAY_STACK_
+
+typedef struct StackNodeType
+{
+	char data;
+} StackNode;
+
+typedef struct ArrayStackType
+{
+    int maxElementCount;
+	int currentElementCount;	// ���� ������ ����
+	StackNode *pTopElement;		// Top ����� ������
+} ArrayStack;
+
+ArrayStack* createArrayStack();
+int pushAS(ArrayStack* pStack, StackNode element);
+StackNode* popAS(ArrayStack* pStack);
+StackNode* peekAS(ArrayStack* pStack);
+void deleteArrayStack(ArrayStack* pStack);
+int isArrayStackFull(ArrayStack* pStack);
+int isArrayStackEmpty(ArrayStack* pStack);
+
+#endif
+
+#ifndef _COMMON_STACK_DEF_
+#define _COMMON_STACK_DEF_
+
+#define TRUE		1
+#define FALSE		0
+
+#endif
